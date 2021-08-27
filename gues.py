ec9218b20b343e8a8d9f132ec30013375508f447
khadija-6 <khadija6.abdi@protonmail.com>
2021-08-27 08:42:52 +0300

    gues projects

---
+++ gues.py
@@ -0,0 +1,45 @@
+import time
+import random
+
+print("My name is khadija\nI use python to write with pograms")
+
+coock = input("gues a number ")
+answer = random,randint(1,5)
+
+if coock == answer:
+    print("correct")
+else:
+    print("incorrect")
+
+
+coock = input("gues a number ")
+answer = random,randint(1,5)
+
+if coock == answer:
+    print("correct")
+else:
+    print("incorrect")
+
+coock = input("gues a number ")
+answer = random,randint(1,5)
+
+if coock == answer:
+    print("correct")
+else:
+    print("incorrect")
+
+coock = input("gues a number ")
+answer = random,randint(1,5)
+
+if coock == answer:
+    print("correct")
+else:
+    print("incorrect")
+
+coock = input("gues a number ")
+answer = random,randint(1,5)
+
+if coock == answer:
+    print("correct")
+else:
+    print("incorrect")
